b06dc515ed471e85977768711d65cff58e6d011b
SalmaMei <140433654+SalmaMei@users.noreply.github.com>
2025-07-25 23:35:48 +0700

    Halaman User

--- user/dashboard.php
+++ user/dashboard.php
@@ -374,7 +374,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 <style>
 body {
-    background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+    background: linear-gradient(135deg, #667eea 0%,rgb(253, 252, 253) 100%);
     min-height: 100vh;
 }
 .card {
